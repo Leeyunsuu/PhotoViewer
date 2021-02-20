ecfd1101c60b08a0f07f3bcc4a1c5de97f2951d9
yunsuteng <75407987+Leeyunsuu@users.noreply.github.com>
2021-02-20 14:30:27 +0900

    Initial Commit

---
+++ PhotoViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoViewer
+//
+//  Created by 이윤수 on 2021/02/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
